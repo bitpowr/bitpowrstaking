8e5701c61b41fa28513c927f39e89ec926acf6d8
Oyetoke Toby <oyetoketoby80@gmail.com>
2023-02-24 01:07:32 +0100

    Updated DB

--- db.sql
+++ db.sql
@@ -1,46 +1,32 @@
+CREATE TABLE "collections" (
+  "id" int PRIMARY KEY,
+  "uid" varchar DEFAULT (uuid_generate_v4()),
+  "created_by" varchar,
+  "collection_uri" varchar,
+  "network" varchar DEFAULT 'devnet',
+  "created_at" datetime,
+  "updated_at" datetime
+);
 
-Table collections {
-  id int [pk]
-  
-  uid varchar [default: `uuid_generate_v4()`] 
+CREATE TABLE "collection_upvotes" (
+  "id" int PRIMARY KEY,
+  "uid" varchar DEFAULT (uuid_generate_v4()),
+  "collection_uid" varchar NOT NULL,
+  "account" varchar NOT NULL,
+  "upvote" boolean DEFAULT false,
+  "created_at" datetime,
+  "updated_at" datetime
+);
 
-  created_by varchar
-  collection_uri varchar
+CREATE TABLE "collection_validators" (
+  "id" int PRIMARY KEY,
+  "uid" varchar DEFAULT (uuid_generate_v4()),
+  "collection_uid" varchar NOT NULL,
+  "account" varchar NOT NULL,
+  "created_at" datetime,
+  "updated_at" datetime
+);
 
-  network varchar [default: "devnet"]
+ALTER TABLE "collection_upvotes" ADD FOREIGN KEY ("collection_uid") REFERENCES "collections" ("uid");
 
-  created_at datetime
-  updated_at datetime
-}
-
-
-Table collection_upvotes {
-  id int [pk]
-  
-  uid varchar [default: `uuid_generate_v4()`] 
-
-  collection_uid varchar [ref: > collections.uid, not null]
-
-  account varchar [not null]
-
-  upvote boolean [default: false] 
-
-  created_at datetime
-  updated_at datetime
-}
-
-
-Table collection_validators{
-  id int [pk]
-  
-  uid varchar [default: `uuid_generate_v4()`] 
-
-  collection_uid varchar [ref: > collections.uid, not null]
-
-  account varchar [not null]
-
-  
-
-  created_at datetime
-  updated_at datetime
-}
+ALTER TABLE "collection_validators" ADD FOREIGN KEY ("collection_uid") REFERENCES "collections" ("uid");
